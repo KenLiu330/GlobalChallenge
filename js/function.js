87d8b457a60912ce78833cb5f806fa4aeebbbe08
Ken Liu (Shek Kin) <sliu@ccsuvt.org>
2016-06-09 12:50:16 -0400

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,1 +1,6 @@
-
+function calcCost() {
+  document.getElementById('output').innerHTML = ("");
+  var totalPower = document.getElementById("totalkWh").value;
+  var totalCost = 0.1690 * totalPower;
+  document.getElementById('output').innerHTML = "Your total cost of electricity per year is $" + totalCost.toFixed(2);
+}
